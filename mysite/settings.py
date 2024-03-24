2a06e784bb234b59d2d5f6e09190cd16dc15928d
KevinKballerO <kballeroolazabal2001@gmail.com>
2024-03-24 17:28:43 -0400

    settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-law4%ge28t0-bsj=xr8@8y%*y%kvvjoalsmm@=ed%s=$(@=f^y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["kevincaballero.pythonanywhere.com"]
 
 
 # Application definition
